46c3c13d3271205af02762ca120369f66e140e22
caggursoy-MacOS <cagatay.guersoy@zi-mannheim.de>
2021-11-30 23:01:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# advent-of-code-2021
+ Attempts of Advent of Code 2021 in Python
+
+*  [Day 1](./day1/advent1.py) waiting!
---
+++ day1/advent1.py
@@ -0,0 +1,2 @@
+with open('day1-input.txt', 'r', encoding='utf-8') as file:
+    inputList = list(map(int, file.readlines()))
